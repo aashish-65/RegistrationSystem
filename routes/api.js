8baba2e3dd2ec60fc891cb2876f31c8ddd77eed9
aashish-65 <116788755+aashish-65@users.noreply.github.com>
2024-10-31 01:08:51 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const userControllers = require('../controllers/userController');
 router.post('/register', userControllers.register);
 router.get('/users', userControllers.getUsers);
 router.get('/users/:id', userControllers.getUser);
+router.get('/users/scan/:id', userControllers.scanUser);
 router.put('/users/update/:id', userControllers.updateUser);
 router.delete('/users/delete/:id', userControllers.deleteUser);
 
