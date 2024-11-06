5f9444e27f6133a0e4f0c7abfa7b397af86e813d
aashish-65 <116788755+aashish-65@users.noreply.github.com>
2024-11-06 12:46:54 +0530

    Update api.js for registrations count

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,7 @@ router.post('/register', userControllers.register);
 router.get('/users', userControllers.getUsers);
 router.get('/users/:id', userControllers.getUser);
 router.get('/users/scan/:id', userControllers.scanUser);
+router.get('/registrations/count', userControllers.getRegistrationCount);
 router.put('/users/update/:id', userControllers.updateUser);
 router.delete('/users/delete/:id', userControllers.deleteUser);
 router.post('/send-email', userControllers.sendEmail);
