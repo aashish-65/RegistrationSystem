03bd4ecca7e883e714dbac2ca1789c1e8b24ebd0
aashish-65 <116788755+aashish-65@users.noreply.github.com>
2024-11-06 12:48:13 +0530

    Update userController.js for registration count.

--- controllers/userController.js
+++ controllers/userController.js
@@ -178,3 +178,12 @@ exports.sendEmail = async (req, res) => {
     res.status(500).json({ message: "Error sending email. Please try again later." });
   }
 };
+
+exports.getRegistrationCount = async (req, res) => {
+  try {
+    const count = await userModel.countDocuments();
+    res.status(200).json({ count });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching registration count" });
+  }
+}
